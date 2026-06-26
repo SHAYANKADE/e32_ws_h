24b417c576ba5ed7aa638c72213564f3ca898e21
SHAYANKADE <shayan.yt.404@gmail.com>
2026-06-26 16:02:19 +0330

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+let gpioState = "OFF";
+
+app.get('/status', (req, res) => {
+  res.send(gpioState);
+});
+
+app.post('/control', (req, res) => {
+  gpioState = req.body.state;
+  res.json({ success: true, state: gpioState });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server roye port ${PORT} ejra shod`);
+});
